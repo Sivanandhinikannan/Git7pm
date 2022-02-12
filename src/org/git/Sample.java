52ad5b8c731a6057ceb084e3c16b98faea9d5a81
Sivanandhinikannan <sweetysiva07@gmail.com>
2022-02-12 19:37:09 +0530

    Mathi done her work

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -2,6 +2,12 @@ package org.git;
 
 public class Sample {
 
+	public void mathi() {
+		System.out.println("Mathi done.....");
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
