01e07bbf6afc2064fbe42e5e70fd0bad44911941
Sivanandhinikannan <sweetysiva07@gmail.com>
2022-02-12 19:26:00 +0530

    Chandru code added

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -2,6 +2,11 @@ package org.git;
 
 public class Sample {
 
+	public void m4() {
+		
+		System.out.println("Chandru.....");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
